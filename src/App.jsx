ec9ddb03c2c3e69d3297bdb2b3526f25657c1d5d
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-10-18 16:03:54 +0600

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Button } from "./components/common/Button";
-import { SectionTitle } from "./components/common/SectionTitle";
 
 const App = () => {
   return (
     <BrowserRouter>
       <main className="font-onest ">
-        <Button>Good</Button>
-        <Button className="bg-red-500">Dark</Button>
-        <SectionTitle>Section Title</SectionTitle>
         <Routes>
-          {/* <Route path="" element={<Navbar />}>
+          <Route path="" element={<Navbar />}>
             <Route path="/" element={<Home />} />
             <Route path="/About" element={<About />} />
             <Route path="/Contact" element={<Contact />} />
             <Route path="/SignUp" element={<SignUp />} />
             <Route path="*" element={<Error />} />
-          </Route> */}
+          </Route>
         </Routes>
       </main>
     </BrowserRouter>
